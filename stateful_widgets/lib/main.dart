3b45e23b608193957d69771a90c1ada3428d27ea
bhyn98 <bhyn9785@gmail.com>
2024-07-29 17:58:02 +0900

    feat: Learn about Life Cycle

--- stateful_widgets/lib/main.dart
+++ stateful_widgets/lib/main.dart
@@ -6,18 +6,23 @@ void main() {
 
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
-
   @override
   State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-  int counter = 0;
+  List<int> numbers = [];
+  bool showTitle = true;
+  void toggleTitle() {
+    setState(() {
+      showTitle = !showTitle;
+    });
+  }
 
   void onPressed() {
     setState(() {
       // build method 재호출
-      counter = counter + 1;
+      numbers.add(numbers.length);
     });
 
 //////꼭 setState() 안에 상태 변화 안넣어도 됨
@@ -30,28 +35,62 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        textTheme: const TextTheme(
+          titleLarge: TextStyle(
+            color: Colors.red,
+          ),
+        ),
+      ),
       home: Scaffold(
         backgroundColor: const Color(0xFFF4EDDB),
         body: Center(
             child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text(
-              'Click Count',
-              style: TextStyle(fontSize: 30),
-            ),
-            Text(
-              '$counter',
-              style: const TextStyle(fontSize: 30),
-            ),
+            showTitle ? const MyLargeTitle() : const Text('nothing'),
             IconButton(
-              onPressed: onPressed,
-              icon: const Icon(Icons.add_box_rounded),
-              iconSize: 40,
+              onPressed: toggleTitle,
+              icon: const Icon(Icons.remove_red_eye),
             )
           ],
         )),
       ),
+    );
+  }
+}
+
+class MyLargeTitle extends StatefulWidget {
+  const MyLargeTitle({
+    super.key,
+  });
+
+  @override
+  State<MyLargeTitle> createState() => _MyLargeTitleState();
+}
+
+class _MyLargeTitleState extends State<MyLargeTitle> {
+  int count = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    print("initState!");
+  }
+
+  @override
+  void disPose() {
+    super.dispose();
+    print('dispose');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print('build!');
+    return Text(
+      'My Large Title',
+      style: TextStyle(
+          fontSize: 30, color: Theme.of(context).textTheme.titleLarge?.color),
     );
   }
 }
