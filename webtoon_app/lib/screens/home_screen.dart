7e1976364b6e1d3eec58e5966ca03f2d65da06f9
bhyn98 <bhyn9785@gmail.com>
2024-08-01 18:08:58 +0900

    fix: fix the widget to StatelessWidget / create ListView.builder for the webtoon list

--- webtoon_app/lib/screens/home_screen.dart
+++ webtoon_app/lib/screens/home_screen.dart
@@ -2,33 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:webtoon_app/models/webtoon_model.dart';
 import 'package:webtoon_app/services/api_service.dart';
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+class HomeScreen extends StatelessWidget {
+  HomeScreen({super.key});
 
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
+// @immutable 메세지 --> final 변수로 변경하기
 
-class _HomeScreenState extends State<HomeScreen> {
-  List<WebtoonModel> webtoons = [];
-  bool isLoading = true;
-
-  void waitForWebToons() async {
-    webtoons = await ApiService.getTodaysToons();
-    isLoading = false;
-    setState(() {});
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    waitForWebToons();
-  }
+// Future(비동기)로 api service 이용하기 때문에 StatefulWidget이 아니라 StatelessWidget 사용해도 된다.
+  final Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
 
   @override
   Widget build(BuildContext context) {
-    print(webtoons);
-    print(isLoading);
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
@@ -43,6 +26,27 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
         ),
       ),
+      body: FutureBuilder(
+          future: webtoons,
+          builder: (context, snapshot) {
+            // snapshot을 이용하면 상태 알 수 있음
+            if (snapshot.hasData) {
+              return ListView.builder(
+                  // ListView.builder : 모든 아이템을 한번에 만드는 대신 만들려는 아이템에 itemBuilder 함수 실행 -> 함수 내에서 build 되는 아이템의 인덱스 접근 가능
+                  scrollDirection: Axis.horizontal,
+                  itemCount: snapshot.data!.length,
+                  // itemBuilder : 함수가 인덱스 0 ~ 10 까지만 호출됨 // Recycle
+                  itemBuilder: (context, index) {
+                    var webtoon = snapshot.data![index];
+                    return Text(webtoon.title);
+                  });
+            }
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }),
+      // Builder은 UI를 그리기 위한 함수
+      // initialData 전달 가능
     );
   }
 }
