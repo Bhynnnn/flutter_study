b9a92173fb2c46afb5d6a28e556909417349fab5
bhyn98 <bhyn9785@gmail.com>
2024-08-01 18:16:27 +0900

    fix: Fix ListView.builder to ListView.separated

--- webtoon_app/lib/screens/home_screen.dart
+++ webtoon_app/lib/screens/home_screen.dart
@@ -27,24 +27,38 @@ class HomeScreen extends StatelessWidget {
         ),
       ),
       body: FutureBuilder(
-          future: webtoons,
-          builder: (context, snapshot) {
-            // snapshot을 이용하면 상태 알 수 있음
-            if (snapshot.hasData) {
-              return ListView.builder(
-                  // ListView.builder : 모든 아이템을 한번에 만드는 대신 만들려는 아이템에 itemBuilder 함수 실행 -> 함수 내에서 build 되는 아이템의 인덱스 접근 가능
-                  scrollDirection: Axis.horizontal,
-                  itemCount: snapshot.data!.length,
-                  // itemBuilder : 함수가 인덱스 0 ~ 10 까지만 호출됨 // Recycle
-                  itemBuilder: (context, index) {
-                    var webtoon = snapshot.data![index];
-                    return Text(webtoon.title);
-                  });
-            }
-            return const Center(
-              child: CircularProgressIndicator(),
+        future: webtoons,
+        builder: (context, snapshot) {
+          // snapshot을 이용하면 상태 알 수 있음
+          if (snapshot.hasData) {
+            // return ListView.builder(
+            //     // ListView.builder : 모든 아이템을 한번에 만드는 대신 만들려는 아이템에 itemBuilder 함수 실행 -> 함수 내에서 build 되는 아이템의 인덱스 접근 가능
+            //     scrollDirection: Axis.horizontal,
+            //     itemCount: snapshot.data!.length,
+            //     // itemBuilder : 함수가 인덱스 0 ~ 10 까지만 호출됨 // Recycle
+            //     itemBuilder: (context, index) {
+            //       var webtoon = snapshot.data![index];
+            //       return Text(webtoon.title);
+            //     });
+
+            return ListView.separated(
+              scrollDirection: Axis.horizontal,
+              itemCount: snapshot.data!.length,
+
+              // itemBuilder : 함수가 인덱스 0 ~ 10 까지만 호출됨 // Recycle
+              itemBuilder: (contexxt, index) {
+                var webtoon = snapshot.data![index];
+                return Text(webtoon.title);
+              },
+              // 사이사이에 widget 넣음
+              separatorBuilder: (context, index) => const SizedBox(width: 20),
             );
-          }),
+          }
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        },
+      ),
       // Builder은 UI를 그리기 위한 함수
       // initialData 전달 가능
     );
