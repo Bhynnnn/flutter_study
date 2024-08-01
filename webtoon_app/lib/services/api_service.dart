2a96b03362e1d1fb527e1972054b8f0b6b8655b8
bhyn98 <bhyn9785@gmail.com>
2024-08-01 17:04:35 +0900

    feat: create webtoon model

--- webtoon_app/lib/services/api_service.dart
+++ webtoon_app/lib/services/api_service.dart
@@ -1,18 +1,28 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
+import 'package:webtoon_app/models/webtoon_model.dart';
 
 class ApiService {
-  final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
-  final String today = "today";
+  static const String baseUrl =
+      "https://webtoon-crawler.nomadcoders.workers.dev";
+  static const String today = "today";
 
-  void getTodaysToons() async {
+  static Future<List<WebtoonModel>> getTodaysToons() async {
+    List<WebtoonModel> webtoonInstances = [];
     // baseUrl + today endpoint 로 URL 만들기
     final url = Uri.parse('$baseUrl/$today');
     // await : 서버에서 요청 처리하고 응답주는거 기다린 후 response에 저장
     final response = await http.get(url);
 
     if (response.statusCode == 200) {
-      print(response.body);
-      return;
+      // type Dynamic == any
+      final List<dynamic> webtoons = jsonDecode(response.body);
+
+      for (var webtoon in webtoons) {
+        webtoonInstances.add(WebtoonModel.fromJson(webtoon));
+      }
+      return webtoonInstances;
     }
     throw Error();
   }
