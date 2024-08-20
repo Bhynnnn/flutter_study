aa38de4c942feaaf100188470cb2d68ea07ec4cb
bhyn98 <bhyn9785@gmail.com>
2024-08-20 18:56:35 +0900

    feat: Create bucketService using provider

---
+++ bucket_list_with_provider/lib/bucket_service.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import 'main.dart';
+
+/// Bucket 담당
+/// BucketService의 bucketList가 변경되는 경우 해당 값을 보여주는 화면들을 갱신시켜 주는 기능을 구현하기 위해
+/// ChangeNotifier 클래스의 기능을 물려 받았습니다.
+/// ChangeNotifier를 상속 받은 경우 notifylisteners();를 호출하여 위젯들을 갱신하는 기능을 사용할 수 있음
+class BucketService extends ChangeNotifier {
+  List<Bucket> bucketList = [
+    Bucket('잠자기', false), // 더미(dummy) 데이터
+  ];
+
+  void createBucket(String job) {
+    bucketList.add(Bucket(job, false));
+  }
+}
