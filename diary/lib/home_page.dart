64930c79606ba14e5bdc88d114d9eb055bab8fd9
bhyn98 <bhyn9785@gmail.com>
2024-09-10 15:22:27 +0900

    feat: CRUD func of diary

--- diary/lib/home_page.dart
+++ diary/lib/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:diary/diary_service.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 import 'package:table_calendar/table_calendar.dart';
 
@@ -13,6 +14,9 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   DateTime selectedDate = DateTime.now();
+  TextEditingController createTextController = TextEditingController();
+  TextEditingController updateTextController = TextEditingController();
+
   @override
   void didChangeDependencies() {
     super.didChangeDependencies();
@@ -23,7 +27,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Consumer<DiaryService>(
       builder: (context, diaryService, child) {
-        List<Diary> diaryList = diaryService.diaryList;
+        List<Diary> diaryList = diaryService.getByDate(selectedDate);
         return Scaffold(
           body: SafeArea(
             child: Column(
@@ -52,7 +56,7 @@ class _HomePageState extends State<HomePage> {
                       color: Colors.indigo,
                       shape: BoxShape.circle,
                     ),
-                    todayDecoration: BoxDecoration(color: Colors.white),
+                    todayDecoration: BoxDecoration(color: Colors.transparent),
                     todayTextStyle: TextStyle(color: Colors.black),
                   ),
                   selectedDayPredicate: (day) {
@@ -63,43 +67,62 @@ class _HomePageState extends State<HomePage> {
                       selectedDate = focusedDate;
                     });
                   },
+                  eventLoader: (date) {
+                    return diaryService.getByDate(date);
+                  },
                 ),
                 Divider(
                   color: Colors.grey.shade200,
                 ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16),
-                  child: ListView.builder(
-                    itemCount: diaryList.length,
-                    itemBuilder: (context, index) {
-                      Diary diary = diaryList[index];
-                      return Row(
-                        children: diaryList.isEmpty
-                            ? [Text('아무것도 없어요')]
-                            : [
-                                Text(
-                                  diary.text,
-                                  style: TextStyle(
-                                    fontSize: 20,
-                                    color: Colors.black,
+                Expanded(
+                  child: diaryList.isEmpty
+                      ? Text(
+                          '아무것도 없어요',
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.black,
+                          ),
+                        )
+                      : ListView.builder(
+                          itemCount: diaryList.length,
+                          itemBuilder: (context, index) {
+                            Diary diary = diaryList[index];
+                            return GestureDetector(
+                              onTap: () {
+                                // 수정
+                                showUpdateDialog(diaryService, diary);
+                              },
+                              onLongPress: () {
+                                showDeleteDialog(diaryService, diary);
+                              },
+                              child: Row(
+                                children: [
+                                  Text(
+                                    diary.text,
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                      color: Colors.black,
+                                    ),
                                   ),
-                                ),
-                                Spacer(),
-                                Text(
-                                  diary.createdAt.toString(),
-                                  style: TextStyle(
-                                      fontSize: 13, color: Colors.grey),
-                                )
-                              ],
-                      );
-                    },
-                  ),
+                                  Spacer(),
+                                  Text(
+                                    DateFormat('kk:mm').format(diary.createdAt),
+                                    style: TextStyle(
+                                        fontSize: 13, color: Colors.grey),
+                                  )
+                                ],
+                              ),
+                            );
+                          },
+                        ),
                 ),
               ],
             ),
           ),
           floatingActionButton: FloatingActionButton(
-            onPressed: () {},
+            onPressed: () {
+              showCreateDialog(diaryService);
+            },
             backgroundColor: Colors.indigo,
             shape:
                 RoundedRectangleBorder(borderRadius: BorderRadius.circular(52)),
@@ -111,5 +134,135 @@ class _HomePageState extends State<HomePage> {
         );
       },
     );
+  }
+
+  void createDiary(DiaryService diaryService) {
+    String newText = createTextController.text.trim();
+    if (newText.isNotEmpty) {
+      diaryService.create(newText, selectedDate);
+      createTextController.text = "";
+    }
+  }
+
+  void updateDiary(DiaryService diaryService, Diary diary) {
+    String updateText = updateTextController.text.trim();
+    if (updateText.isNotEmpty) {
+      diaryService.update(diary.createdAt, updateText);
+    }
+  }
+
+  void showUpdateDialog(DiaryService diaryService, Diary diary) {
+    showDialog(
+        context: context,
+        builder: (context) {
+          updateTextController.text = diary.text;
+          return AlertDialog(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(
+                Radius.circular(5),
+              ),
+            ),
+            title: Text(
+              '일기 수정',
+              style: TextStyle(color: Colors.black, fontSize: 15),
+            ),
+            content: TextField(
+              controller: updateTextController,
+              autofocus: true,
+              decoration: InputDecoration(
+                hintText: '한 줄 일기를 작성해주세요.',
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.indigo),
+                ),
+              ),
+              onSubmitted: (_) {
+                updateDiary(diaryService, diary);
+                Navigator.pop(context);
+              },
+            ),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: Text('취소'),
+              ),
+              TextButton(
+                onPressed: () {
+                  updateDiary(diaryService, diary);
+                  Navigator.pop(context);
+                },
+                child: Text('수정'),
+              ),
+            ],
+          );
+        });
+  }
+
+  void showCreateDialog(DiaryService diaryService) {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(
+                Radius.circular(5),
+              ),
+            ),
+            title: Text(
+              '일기 작성',
+              style: TextStyle(color: Colors.black, fontSize: 15),
+            ),
+            content: TextField(
+              controller: createTextController,
+              autofocus: true,
+              decoration: InputDecoration(hintText: '한 줄 일기를 작성해주세요.'),
+              onSubmitted: (_) {
+                createDiary(diaryService);
+                Navigator.pop(context);
+              },
+            ),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text('취소'),
+              ),
+              TextButton(
+                onPressed: () {
+                  createDiary(diaryService);
+                  Navigator.pop(context);
+                },
+                child: Text('작성'),
+              ),
+            ],
+          );
+        });
+  }
+
+  void showDeleteDialog(DiaryService diaryService, Diary diary) {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text('일기를 삭제하시겠습니까'),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text('취소'),
+              ),
+              TextButton(
+                onPressed: () {
+                  diaryService.delete(diary.createdAt);
+                  Navigator.pop(context);
+                },
+                child: Text('삭제'),
+              ),
+            ],
+          );
+        });
   }
 }
