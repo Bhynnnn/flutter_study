60e58e1a3fd14eef25cee0012870491209678d6b
bhyn98 <bhyn9785@gmail.com>
2024-11-27 18:29:20 +0900

    fix: Fix the code line

--- status_riverpod_example/lib/riverpod/code_generation_provider.dart
+++ status_riverpod_example/lib/riverpod/code_generation_provider.dart
@@ -1,13 +1,12 @@
-import 'dart:ui';
-
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'code_generation_provider.g.dart';
-
+//test1
 // 1. 어떤 provider를 사용할 지 결정할 고민 할 필요 없도록
-// provider? futureProvider? ,,,
+// provider? futureProvider?
 // StateNotifierProvider
 
+//test
 // final _testProvider = Provider((ref) => 'Hello Code Generation');
 
 @riverpod
@@ -17,14 +16,18 @@ String gState(GStateRef ref) {
 
 @riverpod
 Future<int> gStateFuture(GStateFutureRef ref) async {
-  await Future.delayed(Duration(seconds: 3));
+  await Future.delayed(
+    Duration(seconds: 3),
+  );
   return 10;
 }
 
 // keepAlive : 살려둬라 -->  AutoDispose 안되게해라
 @Riverpod(keepAlive: true)
 Future<int> gStateFuture2(GStateFuture2Ref ref) async {
-  await Future.delayed(Duration(seconds: 3));
+  await Future.delayed(
+    Duration(seconds: 3),
+  );
   return 10;
 }
 // 2. Parameter > Family 파라미터를 일반 함수처럼 사용할 수 있도록
--- status_riverpod_example/lib/screen/home_screen.dart
+++ status_riverpod_example/lib/screen/home_screen.dart
@@ -22,65 +22,105 @@ class HomeScreen extends StatelessWidget {
       body: ListView(
         children: [
           ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).push(
-                    MaterialPageRoute(builder: (_) => StateProviderScreen()));
-              },
-              child: Text('StateProviderScreen')),
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (_) => StateProviderScreen(),
+                ),
+              );
+            },
+            child: Text('StateProviderScreen'),
+          ),
           ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).push(MaterialPageRoute(
-                    builder: (_) => StateNotifierProviderScreen()));
-              },
-              child: Text('StateNotifierProviderScreen')),
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (_) => StateNotifierProviderScreen(),
+                ),
+              );
+            },
+            child: Text('StateNotifierProviderScreen'),
+          ),
           ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).push(
-                    MaterialPageRoute(builder: (_) => FutureProviderScreen()));
-              },
-              child: Text('FutureProviderScreen')),
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (_) => FutureProviderScreen(),
+                ),
+              );
+            },
+            child: Text('FutureProviderScreen'),
+          ),
           ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).push(
-                    MaterialPageRoute(builder: (_) => StreamProviderScreen()));
-              },
-              child: Text('StreamProviderScreen')),
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (_) => StreamProviderScreen(),
+                ),
+              );
+            },
+            child: Text('StreamProviderScreen'),
+          ),
           ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).push(
-                    MaterialPageRoute(builder: (_) => FamilyModifierScreen()));
-              },
-              child: Text('FamilyModifierScreen')),
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (_) => FamilyModifierScreen(),
+                ),
+              );
+            },
+            child: Text('FamilyModifierScreen'),
+          ),
           ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).push(MaterialPageRoute(
-                    builder: (_) => AutoDisposeModifierScreen()));
-              },
-              child: Text('AutoDisposeModifierScreen')),
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (_) => AutoDisposeModifierScreen(),
+                ),
+              );
+            },
+            child: Text('AutoDisposeModifierScreen'),
+          ),
           ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).push(
-                    MaterialPageRoute(builder: (_) => ListenProviderScreen()));
-              },
-              child: Text('ListenProviderScreen')),
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (_) => ListenProviderScreen(),
+                ),
+              );
+            },
+            child: Text('ListenProviderScreen'),
+          ),
           ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).push(
-                    MaterialPageRoute(builder: (_) => SelectProviderScreen()));
-              },
-              child: Text('SelectProviderScreen')),
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (_) => SelectProviderScreen(),
+                ),
+              );
+            },
+            child: Text('SelectProviderScreen'),
+          ),
           ElevatedButton(
-              onPressed: () {
-                Navigator.of(context)
-                    .push(MaterialPageRoute(builder: (_) => ProviderScreen()));
-              },
-              child: Text('ProviderScreen')),
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (_) => ProviderScreen(),
+                ),
+              );
+            },
+            child: Text('ProviderScreen'),
+          ),
           ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).push(
-                    MaterialPageRoute(builder: (_) => CodeGenerationScreen()));
-              },
-              child: Text('CodeGenerationScreen')),
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (_) => CodeGenerationScreen(),
+                ),
+              );
+            },
+            child: Text('CodeGenerationScreen'),
+          ),
         ],
       ),
     );
