0b788b566254f61ba7bee4fe52ad6d5a4788900f
bhyn98 <bhyn9785@gmail.com>
2024-12-03 19:27:04 +0900

    feat: Make CursorPagination with riverpod

--- actual/lib/common/model/cursor_pagination_model.dart
+++ actual/lib/common/model/cursor_pagination_model.dart
@@ -2,10 +2,22 @@ import 'package:json_annotation/json_annotation.dart';
 
 part 'cursor_pagination_model.g.dart';
 
+abstract class CursorPaginationBase {}
+
+class CursorPaginationError extends CursorPaginationBase {
+  final String message;
+
+  CursorPaginationError({
+    required this.message,
+  });
+}
+
+class CursorPaginationLoading extends CursorPaginationBase {}
+
 @JsonSerializable(
   genericArgumentFactories: true,
 )
-class CursorPagination<T> {
+class CursorPagination<T> extends CursorPaginationBase {
   final CursorPaginationMeta meta;
   final List<T> data;
 
@@ -13,6 +25,13 @@ class CursorPagination<T> {
     required this.meta,
     required this.data,
   });
+
+  CursorPagination copyWith({
+    CursorPaginationMeta? meta,
+    List<T>? data,
+  }) {
+    return CursorPagination(meta: meta ?? this.meta, data: data ?? this.data);
+  }
 
   factory CursorPagination.fromJson(
           Map<String, dynamic> json, T Function(Object? json) fromJsonT) =>
@@ -29,6 +48,32 @@ class CursorPaginationMeta {
     required this.hasMore,
   });
 
+  CursorPaginationMeta copyWith({
+    int? count,
+    bool? hasMore,
+  }) {
+    return CursorPaginationMeta(
+        count: count ?? this.count, hasMore: hasMore ?? this.hasMore);
+  }
+
   factory CursorPaginationMeta.fromJson(Map<String, dynamic> json) =>
       _$CursorPaginationMetaFromJson(json);
+}
+
+// instance is CursorPagination -->true
+// instance is CursorPaginationRefetching -->true
+// 새로고침 (Base extends 안하는 이유는 이미 리스트가 존재하는 상태에서)
+class CursorPaginationRefetching<T> extends CursorPagination<T> {
+  CursorPaginationRefetching({
+    required super.data,
+    required super.meta,
+  });
+}
+
+// 리스트이 맨 아래로 내려서 추가 데이터 요청하는 중
+class CursorPaginationFetchingMore<T> extends CursorPagination<T> {
+  CursorPaginationFetchingMore({
+    required super.data,
+    required super.meta,
+  });
 }
