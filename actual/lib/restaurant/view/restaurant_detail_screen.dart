f8c9bc4a4abaad468a6ee453d3b25ac549725523
bhyn98 <bhyn9785@gmail.com>
2024-12-06 15:14:12 +0900

    feat: Create rating cardview

--- actual/lib/restaurant/view/restaurant_detail_screen.dart
+++ actual/lib/restaurant/view/restaurant_detail_screen.dart
@@ -1,11 +1,13 @@
 import 'package:actual/common/layout/default_layout.dart';
 import 'package:actual/product/component/product_card.dart';
+import 'package:actual/rating/component/rating_card.dart';
 import 'package:actual/restaurant/component/restaurant_card.dart';
 import 'package:actual/restaurant/model/restaurant_detail_model.dart';
 import 'package:actual/restaurant/model/restaurant_model.dart';
 import 'package:actual/restaurant/provider/restaurant_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:skeletonizer/skeletonizer.dart';
 
 class RestaurantDetailScreen extends ConsumerStatefulWidget {
   final String id;
@@ -42,11 +44,54 @@ class _RestaurantDetailScreenState
       title: '불타는 떡볶이',
       child: CustomScrollView(
         slivers: [
-          renderTop(model: state!),
+          renderTop(model: state),
+          if (state is! RestaurantDetailModel) renderLoading(),
           if (state is RestaurantDetailModel) renderLabel(),
           if (state is RestaurantDetailModel)
             renderProducts(products: state.products),
+          SliverPadding(
+            padding: EdgeInsets.symmetric(horizontal: 16.0),
+            sliver: SliverToBoxAdapter(
+              child: RatingCard(
+                avatarImage: AssetImage('asset/img/logo.codefactory_logo.png'),
+                images: [],
+                rating: 4,
+                email: 'jc@codefactory.ai',
+                content: '마시써요.',
+              ),
+            ),
+          )
         ],
+      ),
+    );
+  }
+
+  SliverPadding renderLoading() {
+    return SliverPadding(
+      padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
+      sliver: SliverList(
+        delegate: SliverChildListDelegate(
+          List.generate(
+            3,
+            (index) => Padding(
+              padding: const EdgeInsets.only(bottom: 8.0),
+              child: Skeletonizer(
+                justifyMultiLineText: false,
+                enabled: true,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: List.generate(
+                    7,
+                    (index) => Container(
+                      padding: EdgeInsets.only(bottom: 5.0),
+                      child: Text('ddddd dddddd'),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
